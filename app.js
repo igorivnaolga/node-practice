854ea18c74ad60c62d0868648abbd2c1f15a74f0
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2024-07-30 01:22:49 +0300

    add server

--- app.js
+++ app.js
@@ -1,36 +1,50 @@
-import { program } from 'commander';
-import { getMovies } from './lesson-2/commander-example/movies/index.js';
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 
-const invokeAction = async ({ action, id, ...data }) => {
-  switch (action) {
-    case 'getAll':
-      const movies = await getMovies();
-      return console.log(movies);
-    case 'getById':
-      const oneMovie = await getMovieById(id);
-      return console.log(oneMovie);
-    case 'add':
-      const newMovie = await addMovie(data);
-      return console.log(newMovie);
-    case 'updateById':
-      const updateMovie = await updateById(id, data);
-      return console.log(updateMovie);
-    case 'deleteById':
-      const deleteMovie = await deleteById(id);
-      return console.log(deleteMovie);
-    default:
-      console.log('unknown action');
-  }
-};
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'));
 
-program
-  .option('-a, --action <type>')
-  .option('-i, --id <type>')
-  .option('-t, --title <type>')
-  .option('-d, --director <type>');
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running. Use our API on port: ${port}`);
+});
 
-program.parse();
+// import { program } from 'commander';
+// import { getMovies } from './lesson-2/commander-example/movies/index.js';
 
-const options = program.opts();
+// const invokeAction = async ({ action, id, ...data }) => {
+//   switch (action) {
+//     case 'getAll':
+//       const movies = await getMovies();
+//       return console.log(movies);
+//     case 'getById':
+//       const oneMovie = await getMovieById(id);
+//       return console.log(oneMovie);
+//     case 'add':
+//       const newMovie = await addMovie(data);
+//       return console.log(newMovie);
+//     case 'updateById':
+//       const updateMovie = await updateById(id, data);
+//       return console.log(updateMovie);
+//     case 'deleteById':
+//       const deleteMovie = await deleteById(id);
+//       return console.log(deleteMovie);
+//     default:
+//       console.log('unknown action');
+//   }
+// };
 
-invokeAction(options);
+// program
+//   .option('-a, --action <type>')
+//   .option('-i, --id <type>')
+//   .option('-t, --title <type>')
+//   .option('-d, --director <type>');
+
+// program.parse();
+
+// const options = program.opts();
+
+// invokeAction(options);
