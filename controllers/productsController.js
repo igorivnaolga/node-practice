248a07897d3cc6bf9f18a7ba04e51f0e3cca6a38
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2024-07-30 03:49:45 +0300

    add deleteProduct, httpError

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,3 +1,4 @@
+import HttpError from '../helpers/httpError.js';
 import * as services from '../services/productsServices.js';
 
 export const getProducts = async (req, res, next) => {
@@ -13,6 +14,19 @@ export const addProduct = async (req, res, next) => {
   try {
     const product = await services.addProduct(req.body);
     res.status(201).json(product);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteProduct = async (req, res, next) => {
+  try {
+    const product = await services.deleteProductById(req.params.id);
+    if (!product) {
+      throw HttpError(400, 'Not found');
+    }
+
+    res.status(204).json();
   } catch (error) {
     next(error);
   }
--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -4,5 +4,5 @@ import * as controllers from '../controllers/productsController.js';
 const productsRouter = Router();
 productsRouter.get('/', controllers.getProducts);
 productsRouter.post('/', controllers.addProduct);
-
+productsRouter.delete('/:id', controllers.deleteProduct);
 export default productsRouter;
